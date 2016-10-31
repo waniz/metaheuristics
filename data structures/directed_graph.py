fc6b326e89ec0c803099bed7dfe872a5ec394e91
DmitryKustikov <waniz@mail.ru>
2016-10-31 15:21:42 +0300

    directed graph data structure

---
+++ data structures/directed_graph.py
@@ -0,0 +1,52 @@
+class DirectedGraph:
+
+    def __init__(self):
+        self.__adjacent = {}
+        self.__vertex_count = 0
+        self.__edges_count = 0
+
+    def add_connection(self, source, destination):
+        if source in self.__adjacent:
+            self.__adjacent[source].append(destination)
+        else:
+            self.__adjacent[source] = [destination]
+            self.__vertex_count += 1
+
+        if destination in self.__adjacent:
+            pass
+        else:
+            self.__adjacent[destination] = []
+            self.__vertex_count += 1
+        self.__edges_count += 1
+
+    def adjacent_nodes(self, source):
+        return self.__adjacent[source]
+
+    def vertex_count(self):
+        return self.__vertex_count
+
+    def edges_count(self):
+        return self.__edges_count
+
+    def vertex_degree(self, source):
+        if source in self.__adjacent:
+            return len(self.__adjacent[source])
+        else:
+            return None
+
+    def vertexes(self):
+        return self.__adjacent.keys()
+
+
+graph = DirectedGraph()
+graph.add_connection('A', 'C')
+graph.add_connection('A', 'D')
+graph.add_connection('B', 'D')
+
+print(graph.vertex_count(), graph.edges_count())
+print(graph.adjacent_nodes('B'))
+print(graph.adjacent_nodes('A'))
+print(graph.vertex_degree('C'))
+print(graph.vertexes())
+
+
